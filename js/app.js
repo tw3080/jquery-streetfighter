63e1494733538d85a3f1fb8be9e6bb752697f45d
Taylor Welling <tw3080@gmail.com>
2016-05-15 20:49:10 -0700

    fixed cool pose animation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   $(".ryu").mouseenter(function() {
-    /* alert("mouse entered .ryu div"); */
     $(".ryu-still").hide();
     $(".ryu-ready").show();
+    $(document).keydown(function(event) {
+      if (event.which == 88) {
+        $(".ryu-ready").hide();
+        $(".ryu-cool").show();
+      }
+    })
+
+    $(document).keyup(function(event) {
+      if (event.which == 88) {
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
+      }
+    })
   })
 
   .mouseleave(function() {
@@ -19,17 +31,40 @@ $(document).ready(function() {
       500,
       function() {
         $(this).hide();
-        $(this).css("left", "540px");
+        $(this).css("left", "540pxx");
       }
     )
   })
 
   .mouseup(function() {
-    console.log("mouseup");
-    $(".ryu-throwing").hide();
-    $(".ryu-ready").show();
-    // ryu goes back to his ready position
-  });
+    $(".ryu-throwing").finish().show().animate(
+      {"left": "0px"},
+      500,
+      function () {
+        $(this).hide();
+        $(this).css("left", "0px");
+        $(".ryu-ready").show();
+      }
+    )
+  })
+
+  $(document).keydown(function(event) {
+    if (event.which == 88) {
+      $(".ryu-still").hide();
+      $(".ryu-cool").show();
+    }
+  })
+
+  $(document).keyup(function(event) {
+    if (event.which == 88) {
+      $(".ryu-cool").hide();
+      $(".ryu-still").show();
+    }
+    if ($(".ryu").is(":hover") === true) {
+      $(".ryu-still").hide();
+      $("ryu-ready").show();
+    }
+  })
 });
 
 function playHadouken () {
